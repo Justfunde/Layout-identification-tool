66dcb51fd19d2a64b919eaafec59d7e04a1e2978
Justfunde <misha2104201@gmail.com>
2023-11-30 23:14:15 +0300

    Implemented layout hash calc

--- src/LitClient/Workers/LayoutWorker.cpp
+++ src/LitClient/Workers/LayoutWorker.cpp
@@ -2,9 +2,37 @@
 
 #include "Include/Crc32.h"
 
+#include <QFile>
+
+#include <fstream>
+#include <sstream>
+
 void
-LayoutWorker::GetCrc(uint32_t Token, const QString& FileName, const QString& PathToSave = QString())
+LayoutWorker::GetCrc(uint32_t Token, const QString& FileName, const QString& PathToSave)
 {
+   LayoutWorker::Status status = Status::Success;
+   QByteArray hashBytea;
+   try{
+      auto fileBinary = GetFileBinary(FileName.toStdString());
+      if(fileBinary.empty()) { }
+
+      const auto hash = Crc32::CalcHash(fileBinary.data(), fileBinary.size());
+      hashBytea = QByteArray(reinterpret_cast<const char*>(&hash), sizeof(hash));
+
+      if(!PathToSave.isEmpty())
+      {
+         QFile f(PathToSave);
+         if(!f.open(QIODevice::WriteOnly)) { }
+         f.write(hashBytea);
+         f.close();
+      }
+
+   }
+   catch(...)
+   {
+      status = Status::Error;
+   }
+   emit CrcReady(Token, hashBytea, status);
 
 }
 
@@ -20,4 +48,20 @@ void
 LayoutWorker::GetGeometrySignaturesGeneration(uint32_t Token, const QString& FileName)
 {
 
+
+}
+
+std::string
+LayoutWorker::GetFileBinary(const std::string& FileName)
+{
+   std::string ret;
+   if(FileName.empty()) { return ret;}
+
+   std::ifstream f(FileName, std::ifstream::in | std::ifstream::binary);
+   if(!f) { return ret;}
+
+   std::stringstream ss;
+   ss << f.rdbuf();
+   ret = ss.str();
+   return ret;
 }
